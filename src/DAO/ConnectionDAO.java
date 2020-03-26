6ff132e6e06a6fd4082f8f0996fead14968b6521
Miguel <miguel.oleo@alu.comillas.edu>
2020-03-26 16:08:53 +0100

    Falta arreglar try catch

--- src/DAO/ConnectionDAO.java
+++ src/DAO/ConnectionDAO.java
@@ -9,7 +9,7 @@ public class ConnectionDAO {
     private static Connection con;
     private ConnectionDAO() throws ClassNotFoundException, SQLException {
         Class.forName("org.mariadb.jdbc.Driver");
-        con = DriverManager.getConnection("jdbc:mariadb://kosmeticsserver.ddns.net:5432/KDB_BETA?user=cliente&password=cliente");
+        con = DriverManager.getConnection("jdbc:mariadb://kosmeticsserver.ddns.net:5432/pat_7?user=pat&password=pat");
     }
     public static ConnectionDAO getInstance() throws SQLException, ClassNotFoundException {
         if(instance == null){
